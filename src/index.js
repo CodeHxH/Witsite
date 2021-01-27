9fbd827a66e9b86e5646d863a0935493fbac1271
Agustin <rodriguezjoseagustin31@gmail.com>
2021-01-26 22:06:15 -0400

    Servidor de node funcionando

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 
 
-//Initialization
+// initialization
 const app = express();
 
 
-//settings
+// settings
 app.set("port", 3000);
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
 
 
-//middleware
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+// middlewares
+app.use(morgan('dev'));
 
 
-//routes
-app.use(require("./routes/index"));
+// routes
+app.use(require("./routes"));
 
 
-//static files
-app.use(express.static(path.join(__dirname,"public")));
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-//start server
-app.listen(app.get("port"), () => {
-  console.log("server on port: ", app.get("port"));
+// listening the Server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
 });
-
-
-
-
-
-
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
-const { Router } = require("express");
-const router = Router();
+const express = require('express');
+const router = express.Router();
 
-
-router.get("/", (req,res) => {
-    res.render("../views/index.html");
-})
-
+router.get('/', (req, res) => {
+  res.render("index.html");
+});
 
 module.exports = router;
