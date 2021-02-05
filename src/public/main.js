b330dc87ee9ce6f992b95ce9ffc5e93973f8b236
Agustin <rodriguezjoseagustin31@gmail.com>
2021-02-04 23:35:09 -0400

    version movil

--- src/public/main.js
+++ src/public/main.js
@@ -1,9 +1,29 @@
+//NAVBAR RESPONSIVE
 const btn = document.querySelector(".menu--btn");
 const navLinks = document.querySelector(".nav--menu");
 const links = document.querySelectorAll(".nav-links li");
 
-
-//NAVBAR RESPONSIVE
 btn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+//SCROLL REVEAL
+ScrollReveal().reveal('.header--container', { delay: 100 });
+ScrollReveal().reveal('.article--web', { delay: 100 });
+ScrollReveal().reveal('.article--branding', { delay: 100 });
+ScrollReveal().reveal('.form--container', { delay: 100 });
+ScrollReveal().reveal('.footer', { delay: 100 });
+ScrollReveal().reveal('.info--container', { delay: 100 });
+ScrollReveal().reveal('.slider--container', { delay: 100 });
+ScrollReveal().reveal('.reveal', { delay: 100 });
+
+//SLIDER AUTO
+var counter = 1;
+setInterval(function(){
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+
+  if (counter > 4){
+    counter = 1;
+  };
+},5000);
