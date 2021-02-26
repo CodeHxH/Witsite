ea078e14ad315a71eae29f4c305ef5e54dad402d
Agustin <rodriguezjoseagustin31@gmail.com>
2021-02-26 11:44:47 -0400

    procesando errores de nodemailer

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -22,15 +22,15 @@ indexCtrl.sendEmailContact = async (req, res) => {
 	const { name, lastname, email, phone, message } = req.body;
 
 	contentHTML = `
-  <h1>Información de Usuario</h1>
-  <ul>
-      <li>Nombre: ${name}</li>
-      <li>Apellido: ${lastname}</li>
-      <li>Email: ${email}</li>
-      <li>Teléfono: ${phone}</li>
-  </ul>
-  <p>${message}</p>
-  `;
+	<h1>Información de Usuario</h1>
+	<ul>
+    <li>Nombre: ${name}</li>
+    <li>Apellido: ${lastname}</li>
+    <li>Email: ${email}</li>
+    <li>Teléfono: ${phone}</li>
+	</ul>
+	<p>${message}</p>
+	`;
 
 	const transporter = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
@@ -45,14 +45,20 @@ indexCtrl.sendEmailContact = async (req, res) => {
 		},
 	});
 
-	const info = await transporter.sendMail({
-		from: '"Witsite user" <onthewitsite@gmail.com>',
-		to: 'rodriguezjoseagustin31@gmail.com',
-		subject: 'Witsite user',
-		html: contentHTML,
-	});
+	const info = await transporter
+		.sendMail({
+			from: '"Witsite user" <onthewitsite@gmail.com>',
+			to: 'rodriguezjoseagustin31@gmail.com',
+			subject: 'Witsite user',
+			html: contentHTML,
+		})
 
-	console.log('Mensaje enviado', info.messageId);
+		.then((info) => {
+			if (info.messageId) {
+				console.log('Sending email');
+			}
+		})
+		.catch((err) => console.log(err));
 
 	res.redirect('/');
 };
@@ -81,43 +87,43 @@ indexCtrl.sendEmailBudget = async (req, res) => {
 	contentHTML = `
     <h1>Solicitud de presupuesto</h1>
     <ul>
-      <li>Nombre del usuario: ${input9}</li>
-      <li>Nombre del proyecto: ${input2}</li>
-      <li>Redes Sociales: ${input5}</li>
-      <li>Empleados: ${input8}</li>
-      <li>Email: ${input10}</li>
-      <li>País: ${input14}</li>
-      <li>Fecha límite: ${input16}</li>
-      </ul>
-      <br>
-      <h3>¿Tu proyecto tiene un logo?</h3>
-      <p>${input3}</p>
-      <br>
-      <h3>¿De qué se trata tu proyecto?</h3>
-      <p>${input1}</p>
-      <br>
-      <h3>¿Tu proyecto tiene una identidad visual?</h3>
-      <p>${input4}</p>
-      <br>
-      <h3>¿Qué es lo que quieres lograr con tu sitio web?</h3>
-      <p>${input6}</p>
-      <br>
-      <h3>¿Cual es el objetivo principal de tu sitio web?</h3>
-      <p>${input7}</p>
-      <br>
-      <h3>¿Tu negocio o empresa tiene algún tipo de filosofía o valores que quieras recalcar?</h3>
-      <p>${input11}</p>
-      <br>
-      <h3>¿Qué diferencia a tu proyecto, negocio y/o servicio de los demás?</h3>
-      <p>${input12}</p>
-      <br>
-      <h3>¿Como nos has conocido?</h3>
-      <p>${input13}</p>
-      <br>      
-      <h3>¿Hay algo más que quieras decirnos?</h3>
-      <p>${input17}</p>
-      <br>
-  `;
+    <li>Nombre del usuario: ${input9}</li>
+    <li>Nombre del proyecto: ${input2}</li>
+    <li>Redes Sociales: ${input5}</li>
+    <li>Empleados: ${input8}</li>
+    <li>Email: ${input10}</li>
+    <li>País: ${input14}</li>
+    <li>Fecha límite: ${input16}</li>
+    </ul>
+    <br>
+    <h3>¿Tu proyecto tiene un logo?</h3>
+    <p>${input3}</p>
+    <br>
+    <h3>¿De qué se trata tu proyecto?</h3>
+    <p>${input1}</p>
+    <br>
+    <h3>¿Tu proyecto tiene una identidad visual?</h3>
+    <p>${input4}</p>
+    <br>
+    <h3>¿Qué es lo que quieres lograr con tu sitio web?</h3>
+    <p>${input6}</p>
+    <br>
+    <h3>¿Cual es el objetivo principal de tu sitio web?</h3>
+    <p>${input7}</p>
+    <br>
+    <h3>¿Tu negocio o empresa tiene algún tipo de filosofía o valores que quieras recalcar?</h3>
+    <p>${input11}</p>
+    <br>
+    <h3>¿Qué diferencia a tu proyecto, negocio y/o servicio de los demás?</h3>
+    <p>${input12}</p>
+    <br>
+    <h3>¿Como nos has conocido?</h3>
+    <p>${input13}</p>
+    <br>      
+    <h3>¿Hay algo más que quieras decirnos?</h3>
+    <p>${input17}</p>
+    <br>
+	`;
 
 	const transporter = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
@@ -132,14 +138,20 @@ indexCtrl.sendEmailBudget = async (req, res) => {
 		},
 	});
 
-	const info = await transporter.sendMail({
-		from: '"Witsite user" <onthewitsite@gmail.com>',
-		to: 'rodriguezjoseagustin31@gmail.com',
-		subject: 'Witsite user',
-		html: contentHTML,
-	});
+	const info = await transporter
+		.sendMail({
+			from: '"Witsite user" <onthewitsite@gmail.com>',
+			to: 'rodriguezjoseagustin31@gmail.com',
+			subject: 'Witsite user',
+			html: contentHTML,
+		})
 
-	console.log('Mensaje enviado', info.messageId);
+		.then((info) => {
+			if (info.messageId) {
+				console.log('Sending email');
+			}
+		})
+		.catch((err) => console.log(err));
 
 	res.redirect('/message');
 };
