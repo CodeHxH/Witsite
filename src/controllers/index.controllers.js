42036250d7cd1f630c90f80c5fe66de2e11dd0db
Agustin <rodriguezjoseagustin31@gmail.com>
2021-06-20 10:00:06 -0400

    commit

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -3,7 +3,40 @@ const nodemailer = require('nodemailer');
 const indexCtrl = {};
 
 indexCtrl.renderIndex = (req, res) => {
-	res.render('index');
+	const proyects = [
+		{
+			number: '01',
+			name: 'E3 2021',
+			img: '/images/proyect1.png',
+			description:
+				'Algunas noticias de los estrenos anunciados en el E3 2021, en un sitio web simple y atractivo.',
+			link: 'https://witsite-e3.herokuapp.com',
+		},
+		{
+			number: '02',
+			name: "Louise's Roses",
+			img: '/images/proyect2.png',
+			description:
+				'Tienda virtual de rosas, esta app cuenta con una sección de registro, inicio de sesión, buscador de productos y formulario de pago.',
+			link: 'https://louises-roses.herokuapp.com',
+		},
+		{
+			number: '03',
+			name: 'Galería Dinámica',
+			img: '/images/proyect3.png',
+			description: 'Galería dinámica con búsqueda por etiquetas y categorías.',
+			link: 'https://codehxh.github.io/Galeria',
+		},
+		{
+			number: '04',
+			name: 'Notes App',
+			img: '/images/proyect4.png',
+			description:
+				'Aplicación de notas con inicio de sesión y registro. Crea, elimina, actualiza y lee tus notas favoritas.',
+			link: 'https://witsite-notes.herokuapp.com',
+		},
+	];
+	res.render('index', { proyects });
 };
 
 indexCtrl.renderAbout = (req, res) => {
